79e34aa232c778946fc96717b8e262ffa0006f5c
Henrique Fraga <113183874+lhrbf@users.noreply.github.com>
2024-09-28 00:00:47 -0300

    Update create_collection.py

--- src/controllers/create_collection.py
+++ src/controllers/create_collection.py
@@ -2,13 +2,9 @@ from pymongo import MongoClient
 from config.conexao_mongodb import get_db
 
 def create_collection():
-    # Conectar ao MongoDB
     client = MongoClient('mongodb://localhost:27017/')
-
-    # Selecionar o banco de dados
     db = client['Contatos']
 
-    # Verificar se a coleção já existe
     get_db()
     if 'listaContatos' not in db.list_collection_names():
         # Criar a coleção
